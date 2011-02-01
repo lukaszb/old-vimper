2c2f6d2f3977392cf25c05c47ca2112b4317dab6
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2011-02-01 13:31:03 +0100

    Added matchit plugin

--- bootstrap.py
+++ bootstrap.py
@@ -51,6 +51,7 @@ VIM_PLUGINS = {
     'fugitive':       'git://github.com/tpope/vim-fugitive.git',
     'git':            'git://github.com/tpope/vim-git.git',
     'ir_black':       'git://github.com/lukaszb/vim-irblack.git',
+    'matchit':        'https://github.com/vim-scripts/matchit.zip.git',
     'nerdcommenter':  'git://github.com/scrooloose/nerdcommenter.git',
     'nerdtree':       'git://github.com/scrooloose/nerdtree.git',
     'supertab':       'git://github.com/ervandew/supertab.git',
