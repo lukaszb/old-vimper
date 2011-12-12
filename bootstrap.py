72dbd8d6802ce509fb96bd9c09daff6cd2e9c9bd
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2011-12-12 17:06:01 +0100

    Seems I've much better results while indenting js/html without web-indent

--- bootstrap.py
+++ bootstrap.py
@@ -68,7 +68,6 @@ VIM_PLUGINS = {
     'supertab':       'git://github.com/ervandew/supertab.git',
     'surround':       'git://github.com/tpope/vim-surround.git',
     'taglist':        'git://github.com/vim-scripts/taglist.vim.git',
-    'web-indent':     'git://github.com/lukaszb/vim-web-indent.git',
     'yankring':       'git://github.com/vim-scripts/YankRing.vim.git',
     'zoomwin':        'git://github.com/vim-scripts/ZoomWin.git',
 }
